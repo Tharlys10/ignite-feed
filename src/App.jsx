b3f76151250c262119e91b535f898f6876a5a1d6
tharlys10 <tharlys2015c@gmail.com>
2022-06-03 13:51:24 -0300

    feat: :sparkles: created component Header
    criado o componente do Header da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header';
+
+import './global.css';
 
 export function App() {
   return (
     <div className="App">
-      <h1>Olá, mundo!</h1>
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+import igniteLogo from '../assets/ignite_logo.svg';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
+      <strong>Ignite Feed</strong>
+    </header>
+  )
+}
