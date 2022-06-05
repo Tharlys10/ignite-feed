f0fc0eabdc9ade69a704e3c35c46c666160a2db8
tharlys10 <tharlys2015c@gmail.com>
2022-06-05 00:30:00 -0300

    feat: :sparkles: created component Comment
    criado o componente de comentarios

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        src="https://github.com/Tharlys10.png"
+        alt="" 
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Tharlys Alves <span>(Você)</span></strong>
+              <time title="04 de Junho de 2022 às 22:00" dateTime="2022-06-04T22:00:00">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp/>
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,8 +1,56 @@
+import { Comment } from '../Comment/Comment';
+
+import styles from './Post.module.css';
+
 export function Post(props) {
   return (
-    <div>
-      <b>{props.author}</b>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            src="https://github.com/Tharlys10.png" 
+          />
+          <div className={styles.authorInfo}>
+            <strong>Tharlys Alves</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="04 de Junho de 2022 às 22:00" dateTime="2022-06-04T22:00:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          👉{' '}<a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a>{' '}
+          <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea 
+          placeholder="Deixe um comentário aqui..."
+        />
+
+        <footer>
+          <button type='submit'>
+            Publicar
+          </button>
+        </footer>
+      </form>
+
+      <div className={styles.commentLint}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
   );
 }
