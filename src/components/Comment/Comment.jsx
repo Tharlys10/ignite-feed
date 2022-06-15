da6b9fa1e245a99d8d430a088add96a2a5a1a808
tharlys10 <tharlys2015c@gmail.com>
2022-06-14 23:23:37 -0300

    refactor: :recycle: refactor function like count comments

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -12,7 +12,7 @@ export function Comment({ content, onDeleteComment }) {
   }
 
   function handleLikeComment() {
-    setLikeCount(likeCount + 1);
+    setLikeCount((state => state + 1));
   }
 
   return (
