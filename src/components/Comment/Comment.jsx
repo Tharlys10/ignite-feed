b6fe8f3e7cb055c54b49f4820c365110ef76f1c8
tharlys10 <tharlys2015c@gmail.com>
2022-06-14 23:13:46 -0300

    feat: :sparkles: implementation handle like comment
    implementado funções para like de um comentario

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,11 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from '../Avatar/Avatar';
 
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [ likeCount, setLikeCount ] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -33,9 +40,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp/>
-            Aplaudir <span>20</span>
+            Aplaudir <span>{ likeCount }</span>
           </button>
         </footer>
       </div>
